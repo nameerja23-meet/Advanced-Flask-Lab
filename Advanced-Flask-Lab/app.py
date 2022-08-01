af4fa3548f2cc3efdf100016131f671b7ee43ce2
nameerja23-meet <nemojabara@gmail.com>
2022-08-01 16:12:25 +0300

    Base requirements commit

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -17,7 +17,7 @@ posts = {
     "https://scontent.ftlv1-1.fna.fbcdn.net/v/t39.30808-6/243021542_10158462822407507_4186737446569136175_n.jpg?stp=cp0_dst-jpg_e15_fr_q65&_nc_cat=101&ccb=1-7&_nc_sid=ed5ff1&_nc_ohc=nHa7N_8RkP0AX92LvUb&_nc_ht=scontent.ftlv1-1.fna&oh=00_AT8kLSHR6V-bvFjpXVVzHXWXviYhBJgkgHANsGlmTojibg&oe=62E08013": "2021 cohort's Y3 Accelerator!",
     "https://media-exp1.licdn.com/dms/image/C4D1BAQFTbpgMk3KTSg/company-background_10000/0/1614595305396?e=1659178800&v=beta&t=OiSIvxsPJiJkArJIzBCKVF0_-yEta9gv1qLVnViU8bo": "MEET graduation!",
     "https://pbs.twimg.com/media/FPvsO6xVkAEcrBm?format=jpg&name=900x900": "#Throwback to one of our favorite #MEETsummer events: #BowlingNight!",
-    "https://pbs.twimg.com/media/FI_UkcnVIAAUvWN?format=jpg&name=medium": "2020 cohort in their Y1 summer!"}
+    "https://pbs.xtwimg.com/media/FI_UkcnVIAAUvWN?format=jpg&name=medium": "2020 cohort in their Y1 summer!"}
 
 
 #####
@@ -25,7 +25,7 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html', image_link=image_link, user_bio=user_bio)
+    return render_template('index.html', image_link=image_link, user_bio=user_bio, posts = posts)
 
 
 @app.route('/about')  # '/' for the default page
